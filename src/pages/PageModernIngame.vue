88b7e3d708810c2a3d84754bef94d986beb25b44
Wenish <Wenish@users.noreply.github.com>
2023-10-03 18:00:42 +0200

    Update PageModernIngame.vue

--- src/pages/PageModernIngame.vue
+++ src/pages/PageModernIngame.vue
@@ -5,7 +5,7 @@
             <div class="grid grid-cols-[1fr_1fr_1fr] justify-center p-2 max-w-6xl mx-auto">
                 <div></div>
                 <div class="grid justify-center">
-                    <div class="inline-grid bg-black/50 py-0.5 px-2 font-bold justify-center items-center text-lg select-none">04:23</div>
+                    <div class="inline-grid bg-black/50 py-0.5 px-2 font-bold justify-center items-center text-lg select-none">{{ timerText }}</div>
                 </div>
                 <div class="grid justify-end">
                     <button class="bg-black/50 hover:bg-black/75 rounded-full p-1 grid justify-center items-center">
@@ -36,10 +36,34 @@
     </div>
 </template>
 <script setup lang="ts">
+import { ref, computed } from 'vue'
 import IconSettings from '../components/IconSettings.vue';
 import Minimap from '../components/Minimap.vue'
 
+const timer = ref(0)
+const timerMinutes = computed(() => {
+    return Math.floor(timer.value / 60);
+})
+const timerSeconds = computed(() => {
+    return timer.value - timerMinutes.value * 60;
+})
 
+const timerText = computed(() => {
+    return str_pad_left(timerMinutes.value.toString(), '0', 2) + ':' + str_pad_left(timerSeconds.value.toString(), '0', 2);
+})
+
+const sleep = (ms: number) => new Promise((r) => setTimeout(r, ms));
+const timerTick = async () => {
+    timer.value++
+    await sleep(1000)
+    timerTick()
+}
+
+timerTick()
+
+function str_pad_left(string: string, pad: string, length: number) {
+  return (new Array(length + 1).join(pad) + string).slice(-length);
+}
 </script>
 <style scoped>
 .ingame-image {
